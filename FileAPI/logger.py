44c2a76e082607751c4ceafd5d0f186b689193eb
tomas13perez <tomasandres.perez@calbaptist.edu>
2021-02-25 18:07:57 -0800

    cleaned up and finished comments for all of FileAPI section.

--- FileAPI/logger.py
+++ FileAPI/logger.py
@@ -2,15 +2,22 @@ import platform
 import datetime
 import os
 
+"""
+The Logger class will create a log file if it does not
+already exist. The log file provides timestamps for
+when a file is parsed by the main program.
+
+@authors: Tomas Perez, Lauren Nelson, Roberto Rodriguez 
+"""
+
 
 class Logger:
     def __init__(self, logfile):
         self.logfile = logfile
-
-        # TODO: Verify logfile is not itself a directory
+        # Verify that the log file is not a directory.
         if os.path.isdir(logfile):
             raise Exception("The path for the log file must be a file, not a directory.")
-        # TODO: Verify directory exists (note file not needed)
+        # Verify directory exists (note file not needed)
         folder = os.path.dirname(logfile)
         if not os.path.dirname(folder):
             raise Exception("The folder for the log file does not exist.")
@@ -21,10 +28,8 @@ class Logger:
         date = "{0}_{1}_{2} {3}:{4}:{5}".format(
             now.year, now.month, now.day,
             now.hour, now.minute, now.second)
-
         text = "{0}/{1}: {2}".format(machine, date, msg)
-        # TODO: Print to console
         print("    log=" + text)
-        # TODO: Append line to log file
+        # Append line to log file
         with open(self.logfile, 'a+') as file_out:
             file_out.write("{0}\n".format(text))
